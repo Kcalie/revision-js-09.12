6c3f13938b4c83cddac7ff21e10710585c22d8fa
KevinHaraux <kevin.haraux@gmail.com>
2022-12-09 12:19:03 +0100

    02

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,4 +24,128 @@ function listAge(min,max){
         console.log("attention probleme avec la valeur de l'age");
     }
 }
+
+function verifLength(element,longueur) {
+    let valeur = element.value;
+    let booleen = false;
+    if(valeur.length >= longueur) {
+        booleen = true;
+    } else {
+        //alert('Ce champ doit contenir'+longueur+' caractères');
+        booleen = false;
+    }
+    //verif la longueure du mdp et on adapte le background en consequence
+    switch(valeur.length) {/*switch fait des cases remplace les if, else etc... */ 
+    case 6:
+        element.style.background = 'red';
+        break
+    case 8:
+        element.style.background = 'orange';
+        break
+    case 12:
+        element.style.background = 'green';
+        break
+}
+    return booleen;
+}
+//fonction qui genere un mdp
+function genereMdp(){
+    // chaine de cara pour le mdp
+    let caracteres = 'azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN0123456789&é_çà)=';
+    // valeur aleatoire entre 0 et 8 pour le mdp
+    let long = Math.floor(Math.random()*8);
+    // longueuer minimum = 8 cara max 16 cara
+    long = 8+long;
+    // on instancie notre variables mdp
+    let motdepasse = '';
+    // genere mdp dans une boucle
+    for(i=0;i<=long;i++){
+        motdepasse+= caracteres.charAt(Math.floor(Math.random()*caracteres.length));
+    }
+    document.getElementById('password1').value = motdepasse;
+    document.getElementById('password2').value = motdepasse;
+}
+function voirMdp(){
+    document.getElementById('password1').setAttribute('type','text');
+    document.getElementById('password2').setAttribute('type','text');
+}
+function cacherMdp(){
+    document.getElementById('password1').setAttribute('type','password');
+    document.getElementById('password2').setAttribute('type','password');
+}
+function affiche5mdp(){
+    voirMdp();
+    let cache = setTimeout('cacherMdp()', 5000);
+}
+function clickreclick(){
+    let password1 = document.getElementById('password1');
+    let password2 = document.getElementById('password2');
+    if(password1.getAttribute('type') == 'text' && password2.getAttribute('type') == 'text') {
+        cacherMdp();
+        document.getElementById('btn-affiche').innerText = 'afficher mot de passe';
+    } else {
+        voirMdp();
+        document.getElementById('btn-affiche').innerText = 'cacher mot de passe';
+    }
+}
+function verifAge(){
+    let age = document.getElementById('age');
+    if(age.value >= 18){
+        document.getElementById('message').innerText = "vous avez l'age necessaire";
+        document.getElementById('submit').removeAttribute('disabled');
+        document.getElementById('submit').innerText = 'Inscription';
+        return true;
+
+    } else {
+        document.getElementById('submit').setAttribute('disabled', 'disabled');
+        document.getElementById('submit').innerText = 'impossible de valider le formulaire';
+        return false;
+    }
+}
+function verifEmail(element){
+    let regex = /^[a-z0-9.-]{4,}@+[a-z0-9.-]{4,}.+[a-z]{2,}$/i
+    // /^ declare le regex [les cara autorisé] {nbre de cara ex: 2, jusqua infini}  pour signifier la regex $/i
+    if(regex.test(element.value)) {
+        // la regex est ok
+        element.style.background = 'green';
+    } else {
+        // la regex ne passe pas
+        element.style.background = 'red';
+    }
+}
+
+function verifForm(){
+    let nom = document.getElementById('nom');
+    let prenom = document.getElementById('prenom');
+    let email = document.getElementById('email');
+    if(nom.value != "" && prenom.value != "" && email.value != "") {
+        // si la personne a + de 18 ans
+        if (verifAge()) {
+            // on verif si les mdp font plus de 8 cara et si ils sont identiques
+            if(verifPassword()){
+                return true;
+            } else {
+                // si les mdp sont pas identiques
+                return false;
+            }
+        } else {
+            // si la personne est mineur 
+            return false;
+        }
+    } else {
+        // si un des champs n'est pas rempli 
+        if(nom.value == '') {
+            document.getElementById('message').innerText = "veuillez renseignez votre nom";
+        }
+        else if(prenom.value == '') {
+            document.getElementById('message').innerText = "veuillez renseignez votre prenom";
+        }
+        else if(email.value == '') {
+            document.getElementById('message').innerText = "veuillez renseignez votre email";
+        }
+        return false;    
+    }
+
+}
 listAge(12,70);
+verifAge();
